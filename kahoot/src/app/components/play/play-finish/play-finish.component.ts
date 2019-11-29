636451877cb6f38ab8d7ca711d62caab389d078a
Pol Ribas <polribas@bisual.net>
2019-11-29 15:34:32 +0100

    Components structurej + routing

---
+++ kahoot/src/app/components/play/play-finish/play-finish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-finish',
+  templateUrl: './play-finish.component.html',
+  styles: []
+})
+export class PlayFinishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ kahoot/src/app/components/play/play-start/play-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-play-start',
+  templateUrl: './play-start.component.html',
+  styles: []
+})
+export class PlayStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
