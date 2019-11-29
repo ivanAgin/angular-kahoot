3f9f842a4e912feabc9c9729f25d620ed79f33cd
Koubura <ivanaguilarni@gmail.com>
2019-11-29 10:35:32 +0100

    WIP

--- kahoot/src/app/app.module.ts
+++ kahoot/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+/* FIREBASE */
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth'
+import { AngularFireDatabaseModule, AngularFireDatabase} from 'angularfire2/database';
+import { AngularFireModule } from 'angularfire2';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProvaComponent } from './components/prova/prova.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProvaComponent,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule,
+    AngularFireDatabase,
+    AngularFireModule,
+    AngularFireAuth
   ],
   imports: [
     BrowserModule,
---
+++ kahoot/src/app/components/prova/prova.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prova',
+  templateUrl: './prova.component.html',
+  styleUrls: ['./prova.component.css']
+})
+export class ProvaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
