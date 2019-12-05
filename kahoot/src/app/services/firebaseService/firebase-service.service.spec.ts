2cd44b2492d5a214efefab989b0a5c92ea1966b3
Koubura <ivanaguilarni@gmail.com>
2019-12-05 16:16:04 +0100

    Bugs fixed

--- kahoot/src/app/services/firebaseService/firebase-service.service.spec.ts
+++ kahoot/src/app/services/firebaseService/firebase-service.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
+import { FirebaseService } from './firebase-service.service';
 
-import { FirebaseServiceService } from './firebase-service.service';
 
-describe('FirebaseServiceService', () => {
+describe('FirebaseService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: FirebaseServiceService = TestBed.get(FirebaseServiceService);
+    const service: FirebaseService = TestBed.get(FirebaseService);
     expect(service).toBeTruthy();
   });
 });
