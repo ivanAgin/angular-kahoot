d3da4b78f88c93349ecb177ac9041daea642b35d
joanprieto <joanprieto4@gmail.com>
2019-12-08 17:30:09 +0100

    Añadiendo filtres i control de errores en el crear partida de admin con un set de preguntas determinados.

--- kahoot/src/app/components/admin/admin-start/admin-start.component.ts
+++ kahoot/src/app/components/admin/admin-start/admin-start.component.ts
@@ -16,6 +16,9 @@ export class AdminStartComponent implements OnInit {
   partida:Partida;
   id_partida:number = Math.floor(Math.random() * 9999) + 1000;
   ref_key;
+  nombre;
+  questions: string[] = [];
+  set: string = ""
   
   constructor(private activatedRoute: ActivatedRoute,
     private firebase:FirebaseService,
@@ -23,7 +26,23 @@ export class AdminStartComponent implements OnInit {
     private game:GameService) { }
 
   ngOnInit() {
-      this.firebase.createPartida(this.id_partida.toString(), "holii", "set_1", -1).then(
+    this.firebase.getSetsQuestions().subscribe( (q) => {
+      q.forEach( question => {
+        this.questions.push(question.key)
+      })
+    })
+  }
+
+  iniciPartida(){
+    this.firebase.changeState(this.ref_key,0);
+    console.log(this.id_partida);
+    this.router.navigateByUrl(`/admin/${this.ref_key}/game`);
+  }
+
+  createPartida(){
+    console.log(this.set)
+    if(this.set && this.nombre){
+      this.firebase.createPartida(this.id_partida.toString(), this.nombre, this.set, -1).then(
         ref => {
           this.ref_key = ref.key; 
           this.game.setPartida(this.ref_key);
@@ -31,14 +50,11 @@ export class AdminStartComponent implements OnInit {
             data => {
               this.partida = data;
               console.log(data);
-          }
+            }
           );
-        })
-  }
-  iniciPartida(){
-    this.firebase.changeState(this.ref_key,0);
-    console.log(this.id_partida);
-    this.router.navigateByUrl(`/admin/${this.ref_key}/game`);
+        }
+      )
+    }
   }
 
 }
